a57574371ea4e8c8419d481c6bd42fbbc7cfbe33
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-07-14 20:13:07 -0300

    add: short-url component

---
+++ src/app/components/input-url/short-url/short-url.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-url',
+  templateUrl: './short-url.component.html',
+  styleUrls: ['./short-url.component.css']
+})
+export class ShortUrlComponent {
+
+}

a1586aabb86a25836bdc9573d6ad9bb304fe7c9e
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-07-14 20:29:03 -0300

    add: required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { InputUrlComponent } from './components/input-url/input-url.component';
+import { ShortUrlComponent } from './components/input-url/short-url/short-url.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputUrlComponent
+    InputUrlComponent,
+    ShortUrlComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

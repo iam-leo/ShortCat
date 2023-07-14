b08e4c2cbf8d05c56ef7b823c82064daa935bd26
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-07-14 20:26:14 -0300

    add: short-url component in the input-url component

--- src/app/components/input-url/input-url.component.ts
+++ src/app/components/input-url/input-url.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShorturlService } from 'src/app/services/shorturl.service';
 
 @Component({
   selector: 'app-input-url',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-url.component.css']
 })
 export class InputUrlComponent {
+  nombreURL = '';
+  shortURL = '';
+  processURL = false;
 
+  constructor(private _shortUrlService: ShorturlService){ }
+
+  procesarURL(){
+    this._shortUrlService.getUrlShort(this.nombreURL)
+      .subscribe(data => {
+        console.log(data);
+        
+      })
+  }
 }
